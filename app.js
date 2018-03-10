69e37c05379b3107640f73c86d347f643f2f1e4f
Ivette <car16020@uvg.edu.gt>
2018-03-09 19:08:20 -0600

    Correccion app

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express'),
 	path = require('path'),
-	badyParser = require('consolidate'),
+	bodyParser = require('body-parser'),
+	cons = require('consolidate'),
 	dust = require('dustjs-helpers'),
 	pg = require('pg'),
 	app = express();
@@ -8,4 +9,25 @@ var express = require('express'),
 //Coneccion con postgres
 var conString = "postgres://CRM:holi1234@localhost/Proyecto1";
 
+//Asignar el engine de Dust a los archivos .dust
 app.engine('dust',cons.dust);
+
+//Default ext .dust
+app.set('view engine', 'dust');
+app.set('views', __dirname + '/views');
+
+//Public folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+//Body parser Middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
+
+app.get('/', function(req, res){
+	console.log('TEST');
+});
+
+//Server
+app.listen(3000, function(){
+	console.log('Iniciar server en el puerto 3000');
+});
