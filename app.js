3ee4de32e0de4b03993436f1f75056646f06c4db
Yasmin Chavez <ychavez2014ig@gmail.com>
2018-03-20 13:27:23 -0600

    mostrar datos

--- app.js
+++ app.js
@@ -1,11 +1,97 @@
-var express = require('express'),
-	path = require('path'),
-	badyParser = require('consolidate'),
-	dust = require('dustjs-helpers'),
-	pg = require('pg'),
-	app = express();
+let express = require('express'),
+	//path = require('path'),
+	bodyParser = require('body-parser'),
+	morgan = require('morgan'),
+	pg = require('pg');
+	
+// var server = require('http').Server(app); 
+// var io = require('socket.io')(server); 
+// var massive = require("massive");
 
-//Coneccion con postgres
-var conString = "postgres://CRM:holi1234@localhost/Proyecto1";
+// var startExpress = function() {
+	// server.listen(config.express.port); db = app.get('db');
+// }
 
-app.engine('dust',cons.dust);
+let pool = new pg.Pool({
+  host: 'localhost',
+  user: 'CRM',
+  database: 'Proyecto1',
+  password: 'holi1234',
+  port: 5432
+ })
+
+let app = express();
+
+// Body Parser middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(morgan('dev')); 
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+/*
+pool.connect(); 
+pool.query('LISTEN "changes"'); 
+pool.on('notification', function(data) { io.emit("change"); });
+
+var update = function(request, res, next) { var newDoc = request.body.data;
+ db.steps.saveDoc({id:1,data:newDoc}, function(err,response){ if (err) { handleError(err) }; 
+res.json({ data: response }); 
+pool.query('NOTIFY "change"'); }); } 
+
+*/
+
+app.get('/api/Medico', function(request, response){
+	pool.connect(function(err,db,done){
+		if(err){
+			return response.status(400).send(err)
+		} else{
+			db.query('SELECT id_medico as id, nombre, especialidad FROM "Medico"', function(err, table){
+				done();
+
+				if(err){
+					return response.status(400).send(err)
+				}
+				else{
+					return response.status(200).send(table.rows)
+				}
+			})
+		}
+	});
+})
+
+app.post('/api/mediconuevo', function(request, response) {
+	console.log('Adentro de endpoint');
+	pool.connect(function(err, db, done) {
+		console.log('Adentro de callback de postgres');
+		if (err) {
+			return response.status(400).send(err);
+		}
+		
+		db.query(
+			'INSERT INTO "Medico" (nombre, especialidad) VALUES ($1, $2)',
+			[ request.body.nombre, request.body.especialidad ],
+			(err, table) => {
+				console.log('Adentro de callback de insert');
+				done();
+
+				if (err){
+					return response.status(400).send(err);
+				}
+				
+				console.log('DATA INSERTED');
+				response.status(201).send({message:'Data inserted!'});
+			}
+		);
+	});
+});
+
+// Server
+app.listen(3000, function(){
+	console.log('Server Started on port 3000');
+});
---
+++ my-app/src/App.js
@@ -0,0 +1,103 @@
+import React, { Component } from 'react';
+import './App.css';
+import fetch from 'node-fetch';
+
+class App extends Component {
+  
+	constructor() {
+		super();
+		this.state = {
+			medicos: []
+		}
+	}
+
+	// Make ajax calls here
+	componentDidMount() {
+		console.log('component has mounted');
+
+		/*
+		function() { instance = this; 
+		downloadData(); 
+		startSocket(); 
+		socket.on('change', function (data) { 
+				downloadData(); 
+				console.log(data);
+			});
+		} */
+
+		var that = this;
+
+		fetch('http://localhost:3000/api/Medico')
+			.then(function(response){
+				console.log(response)
+				response.json()
+					.then(function(data){
+						that.setState({
+							medicos: data						
+						});
+					})		
+			});
+	} 
+
+	addMedico(event) {
+		var that = this;
+
+		event.preventDefault();
+
+		let medico_data = {
+			nombre: this.refs.medico_nombre.value,
+			especialidad: this.refs.medico_especialidad.value
+		};
+
+		let medicos = that.state.medicos;
+
+		medicos.push(medico_data);
+
+		console.log(medicos);
+
+		that
+			.setState({
+				medicos: medicos
+			})
+			//xmlhttprequest()			
+			fetch('http://localhost:3000/api/mediconuevo', {
+				method: 'POST',
+				headers: {'Content-Type': 'application/json'},
+				body: JSON.stringify(medico_data)
+			})
+			.then(function(response){
+				response.json()
+					.then(function(data){	
+				})
+			})
+			.catch(function(err){
+				console.log(err)
+			});
+	}
+
+	render() {
+		let medicos = this.state.medicos;
+
+		return (
+			// <meta charset="utf-8">
+			<div className="App">
+				<h1> Medi Lock </h1>
+				<form ref="medicosForm">
+					<input type="text" ref="medico_nombre" placeholder="Nombre"/>
+					<input type="text" ref="medico_especialidad" placeholder="Especialidad"/>
+					<button onClick={this.addMedico.bind(this)}>ADD MEDICO</button>
+				</form>
+				<div>
+					{
+						medicos.map(
+							medico =>
+								<li>{medico.nombre} {medico.especialidad}</li>
+						)
+					}
+				</div>
+			</div>
+		);
+	}
+}
+
+export default App;
